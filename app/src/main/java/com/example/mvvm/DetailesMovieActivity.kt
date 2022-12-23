e8ae427b2e35ed85f0a849779d7b16f76e2fefd2
BANCF <91019287+BANCF@users.noreply.github.com>
2022-12-23 16:49:35 +0700

    MVvM Retrofit

---
+++ app/src/main/java/com/example/mvvm/DetailesMovieActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailesMovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailes_movie)
+    }
+}
---
+++ app/src/main/java/com/example/mvvm/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.mvvm.utils
+
+object Constants {
+    const val API_KEY = ""
+    const val BASEURL = ""
+    const val POSTER_BASEURL = ""
+}
